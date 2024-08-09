d42a1a98e185e7b3a56bcde4e8fbb0776e1da05d
Shridhar-Pattar <148942399+Shridhar-Pattar@users.noreply.github.com>
2024-08-09 23:51:47 +0530

    update

--- frequency_of_elements_in_an_array/Main.java
+++ frequency_of_elements_in_an_array/Main.java
@@ -5,17 +5,20 @@ public class Main {
         int[] a = { 1, 2, 1, 3, 5, 6, 7, 3, 2, 1 };
         int n = a.length;
 
-        int count = 0;
+        // To keep track of elements already processed
+        boolean[] visited = new boolean[n];
+
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (a[i] == a[j]) {
-                    count = count + 1;
+            if (!visited[i]) {
+                int count = 1;
+                for (int j = i + 1; j < n; j++) {
+                    if (a[i] == a[j]) {
+                        count++;
+                        visited[j] = true; // Mark as visited
+                    }
                 }
-
+                System.out.println(a[i] + " appears " + count + " times");
             }
-            System.out.println(a[i] + "the frequency is " + count);
-
         }
-
     }
 }
