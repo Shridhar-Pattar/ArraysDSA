a79a491a8b434633f89d3fbc9f46e3bf30b56c98
Shridhar-Pattar <148942399+Shridhar-Pattar@users.noreply.github.com>
2024-08-10 00:31:35 +0530

    commit

--- frequency_of_elements_in_an_array/Main.java
+++ frequency_of_elements_in_an_array/Main.java
@@ -1,5 +1,7 @@
 package frequency_of_elements_in_an_array;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] a = { 1, 2, 1, 3, 5, 6, 7, 3, 2, 1 };
@@ -21,4 +23,26 @@ public class Main {
             }
         }
     }
+    /*
+     * public static void main(String[] args) {
+     * int[] a = { 1, 2, 1, 3, 5, 6, 7, 3, 2, 1 };
+     * Arrays.sort(a);
+     * System.out.println(Arrays.toString(a));
+     * int n = a.length;
+     * int count = 1;
+     * for (int i = 1; i < n; i++) {
+     * if (a[i] == a[i - 1]) {
+     * count++;
+     * } else {
+     * System.out.println(a[i - 1] + " appears " + count + " times");
+     * count = 1;
+     * 
+     * }
+     * 
+     * }
+     * System.out.println(a[n - 1] + " appears " + count + " times");
+     * 
+     * }
+     */
+
 }
